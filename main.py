75f5b3d5d58bf579be5b2ba1f7daf603f98537fe
Prochyxd <Adamoso13MC@gmail.com>
2023-12-22 15:51:03 +0100

    IT WORKS AND DOES STUFF
    I CANT BELIEVE IT BUT IT GENERATES, EVALUATES AND STUFF LIKE THAT

--- main.py
+++ main.py
@@ -1,35 +1,145 @@
-import time
-import config
 import yaml
-from generator import generate_schedule
+import random
+import time
+import multiprocessing
+
+def generate_daily_schedule(subjects, min_hours, max_hours, max_empty_hours):
+    num_hours = random.randint(min_hours, max_hours)
+    max_empty_hours = min(num_hours - 1, max_empty_hours)
+
+    # Determine the number of empty slots (at most one)
+    num_empty_slots = min(max_empty_hours, 1)
+
+    # Create a list of subjects and empty slots
+    schedule_items = subjects + ['____'] * num_empty_slots
+
+    # Shuffle the list
+    random.shuffle(schedule_items)
+
+    # Trim the list to the desired number of hours
+    daily_schedule = schedule_items[:num_hours]
+
+    return daily_schedule
+
+def has_duplicate_hours(schedule):
+    hours_set = set()
+    for hour in schedule:
+        if hour in hours_set:
+            return True
+        if hour != '____':
+            hours_set.add(hour)
+    return False
+
+def evaluate_schedule(schedule, evaluation_criteria):
+    if has_duplicate_hours(schedule):
+        return evaluation_criteria['duplicate_hours_penalty']
+
+    total_hours = sum(len(subjects) for day, subjects in schedule)
+    
+    # Evaluate based on the total hours per day
+    if total_hours == 6 or total_hours == 7:
+        return evaluation_criteria['6_7_hours_bonus']
+    elif total_hours == 8:
+        return evaluation_criteria['8_hours_penalty']
+    elif total_hours == 9:
+        return evaluation_criteria['9_hours_penalty']
+    elif total_hours == 10:
+        return evaluation_criteria['10_hours_penalty']
+
+    return 0  # Default score
+
+def generate_schedule_worker(config, output_queue, progress_counter, exit_flag, best_schedule_lock, best_schedule, evaluation_criteria):
+    while not exit_flag.value:
+        schedule = {}
+        for day in config['days']:
+            daily_schedule = generate_daily_schedule(
+                config['subjects'],
+                config['hours_per_day']['min'],
+                config['hours_per_day']['max'],
+                config['max_empty_hours_per_day']
+            )
+            schedule[day] = daily_schedule
+
+        # Convert the schedule to a hashable format
+        schedule_tuple = tuple((day, tuple(subjects)) for day, subjects in schedule.items())
+
+        score = evaluate_schedule(schedule_tuple, evaluation_criteria)
+
+        with best_schedule_lock:
+            current_best_score = evaluate_schedule(best_schedule[0][0], evaluation_criteria) if best_schedule else float('-inf')
+            if score > current_best_score:
+                best_schedule[:] = [(schedule_tuple, score)]
+
+        output_queue.put(schedule)
+        with progress_counter.get_lock():
+            progress_counter.value += 1
 
 def main():
     with open("config.yaml", "r") as config_file:
         config_data = yaml.safe_load(config_file)
 
+    num_processes = multiprocessing.cpu_count()
+
+    output_queue = multiprocessing.Queue()
+    progress_counter = multiprocessing.Value('i', 0)
+    exit_flag = multiprocessing.Value('i', 0)
+    best_schedule_lock = multiprocessing.Lock()
+    best_schedule = multiprocessing.Manager().list()
     
-    # Generate a random schedule
-    schedule = generate_schedule(config_data)
-    for day, subjects in schedule.items():
-        print(f"{day}: {', '.join(subjects)}")
+    # Define your evaluation criteria here
+    evaluation_criteria = {
+        'duplicate_hours_penalty': -1000,
+        '6_7_hours_bonus': 100,
+        '8_hours_penalty': -1,
+        '9_hours_penalty': -100,
+        '10_hours_penalty': -250
+        # Add more criteria as needed
+    }
 
-    #Generate as many schedules as possible in one minute
-    num_schedules = 0
+    processes = [
+        multiprocessing.Process(
+            target=generate_schedule_worker,
+            args=(config_data, output_queue, progress_counter, exit_flag, best_schedule_lock, best_schedule, evaluation_criteria)
+        )
+        for _ in range(num_processes)
+    ]
+
+    for process in processes:
+        process.start()
+
+    # Watchdog loop
     start_time = time.time()
+    while time.time() - start_time < 20:
+        with progress_counter.get_lock():
+            progress = progress_counter.value
+        print(f"Progress: {progress} schedules generated")
+        time.sleep(1)
 
-    while True:
-        schedule = generate_schedule(config_data)
-        num_schedules += 1
+    # Set exit flag to terminate processes
+    with exit_flag.get_lock():
+        exit_flag.value = 1
 
-        elapsed_time = time.time() - start_time
-        if elapsed_time >= 180:
-            break
+    # Wait for processes to finish
+    for process in processes:
+        process.join(timeout=1)  # Add a timeout to prevent indefinite waiting
 
-    print(f"Generated {num_schedules} schedules in {elapsed_time} seconds")
+    # Find the best schedule among the generated ones
+    best_schedule_list = best_schedule[:]
+    best_schedule_list.sort(key=lambda x: x[1], reverse=True)
+    
+    # Print the best schedule
+    print("Best Schedule:")
+    if best_schedule_list:
+        best_schedule_dict, best_score = best_schedule_list[0]
+        for day, subjects in best_schedule_dict:
+            print(f"{day}: {', '.join(subjects)}")
+        print(f"Score: {best_score}")
+    else:
+        print("No schedules generated.")
+        
+    with progress_counter.get_lock():
+        generated_schedules = progress_counter.value
+    print(f"Generated {generated_schedules} schedules in 20 seconds")
 
 if __name__ == "__main__":
     main()
-
-
-
-    #Generated 8694215 schedules in 180.0014967918396 seconds
